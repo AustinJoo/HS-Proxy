9f27a1fde3c035b2990a3ec52fca816a69d62dee
Austin Joo <joojinhyung@gmail.com>
2019-02-13 11:40:00 -0500

    Cleaned up console.logs

--- server.js
+++ server.js
@@ -17,32 +17,29 @@ app.use(bodyParser.urlencoded({
 }))
 
 app.use((req, res, next) => {
-    // console.log(req.url);
     listingID = req.url.slice(1);
     next();
 })
-
-// app.use('/api/items', proxy({
-//     target: ''
-// }));
-
-// app.use('/api/pictures/:listingID', proxy({
-//     target: 'http://localhost:5050/api/pictures'
-//     target: 'http://ec2-3-16-208-224.us-east-2.compute.amazonaws.com'
-// }));
 
 app.get(`/${loaderKey}`, (req,res) => {
     res.send(loaderKey);
 })
 
+app.use('/api/items/:listingID', proxy({
+    target: 'http://ec2-3-16-57-68.us-east-2.compute.amazonaws.com'
+}));
+
+app.use('/api/booking/:listingID', proxy({
+    target: 'http://ec2-18-216-133-115.us-east-2.compute.amazonaws.com'
+}))
+
 app.get('/:listingID', (req, res) => {
     console.log('API CAROUSEL ENDPOINT IS: ', listingID);
-    // axios.get(`http://localhost:5050/api/pictures/${listingID}`)
+    // NginX url
     // axios.get(`ec2-18-188-37-54.us-east-2.compute.amazonaws.com/api/pictures/${listingID}`)
     axios.get(`http://ec2-3-16-208-224.us-east-2.compute.amazonaws.com/api/pictures/${listingID}`)
     .then((response) => {
         let data = response.data;
-        // console.log('THIS IS DATA: ', data);
         let carouselHTML = data[0];
         let carouselProps = data[1];
         let carouselCSS = data[2];
@@ -56,30 +53,43 @@ app.get('/:listingID', (req, res) => {
                     <title>SSRDocument</title>
                     <<script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
                     <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-                    <style type="text/css">${carouselCSS}</style>
+                    <style type="text/css" id="carouselCSS">${carouselCSS}</style>
+                    <link rel="stylesheet" type="text/css" media="screen" href="http://ec2-3-16-57-68.us-east-2.compute.amazonaws.com/style.css"/>
+                    <link rel="stylesheet" type="text/css" media="screen" href="http://ec2-18-216-133-115.us-east-2.compute.amazonaws.com/style.css"/>
                     </head>
                     <body>
+                    <div id="Components container">
+                    <div>
                     <div id="image-carousel">${carouselHTML}</div>
+                    </div>
+                    <div>
+                    <div id="app"></div>
+                    </div>
+                    <div id="applex"></div>
+                    </div>
                     
                     <script src="http://ec2-3-16-208-224.us-east-2.compute.amazonaws.com/bundle/bundle.js"></script>
+                    <script src="http://ec2-18-216-133-115.us-east-2.compute.amazonaws.com/dist/bundle.js"></script>
+                    <script src="http://ec2-3-16-57-68.us-east-2.compute.amazonaws.com/dist/bundle.js"></script>
                     <script>
-                        ReactDOM.hydrate(
-                            React.createElement(ImageCarousel, ${JSON.stringify(carouselProps)}),
-                            document.getElementById('image-carousel')
-                        );
+                    ReactDOM.hydrate(
+                        React.createElement(ImageCarousel, ${JSON.stringify(carouselProps)}),
+                        document.getElementById('image-carousel')
+                    );
+                    ReactDOM.render(
+                        React.createElement(BookingModule),
+                        document.getElementById('applex')
+                    );
+                    ReactDOM.render(
+                        React.createElement(ReviewsModule),
+                        document.getElementById('app')
+                    );
                     </script>
                 </body>
             </html>`
         )
     })
 });
-
-{/* <script src="Justin'sComponent/bundle.js"></script> */}
-{/* <script src="Alexa'sComponent/bundle.js"></script> */}
-
-// app.use('/api/booking', proxy({
-//     target: ''
-// }))
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
